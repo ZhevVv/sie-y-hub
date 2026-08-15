cb9f6720e52632e988dd049acac118db7c447c75
Zhevv <162014418+ZhevVv@users.noreply.github.com>
2026-08-15 11:06:20 +0800

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,17 +1,6 @@
 --[[
     ================================================================
-    [ SCRIPT INFORMATION ]
-    Project: Custom Script
-    Author: OYB
-    YouTube: https://www.youtube.com/channel/UCAlXXV1Hbvf7WbfXARuVtiQ
-    
-    [ TERMS AND CONDITIONS ]
-    - You ARE allowed to use and modify this script for your own games.
-    - You ARE NOT allowed to re-upload, redistribute, or claim 
-      ownership of this script.
-    - Removing or altering these credits is strictly prohibited.
-    
-    Copyright (c) 2026 OYB. All rights reserved.
+    THIS SCRIPT IS OBFUSCATED.
     ================================================================
 ]]
 
